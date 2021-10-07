7b9b6dce671df37ffb78c6c7310af07847d50d6c
Aixzyl14 <71727630+Aixzyl14@users.noreply.github.com>
2021-10-07 10:22:33 +0100

    Add files via upload

---
+++ code/TestAix/Neural_Network.py
@@ -0,0 +1,20 @@
+import numpy
+from numpy.lib.financial import nper
+
+
+import numpy as np
+input_value = np.array([[0,0], [0,1],[1,1],[1,0]])
+input_value.shape
+input_value
+
+output = np.array([0,1,1,0])
+output = output.reshape(4,1)
+
+weights = np.array([[0.1],[0.2]])
+print(weights)
+
+
+
+
+
+

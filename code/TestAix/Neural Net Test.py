99b7ac7306648ff06a9a1e62fff3f65efa5e8680
Aixzyl14 <71727630+Aixzyl14@users.noreply.github.com>
2021-10-13 10:59:14 +0100

    Update Neural Net Test.py

--- code/TestAix/Neural Net Test.py
+++ code/TestAix/Neural Net Test.py
@@ -1,19 +1,56 @@
 #cmd
-#pip  install numpy
+# pip  install numpy
+# pip install nnfs
+import math
+
 import sys
 import numpy as np
 import matplotlib
+import nnfs
+from nnfs.datasets import spiral_data
 
 np.random.seed(0)
+
+# nnfs.init()
+
+# layer_outputs = [[4.8, 1.21, 2.385],
+#                 [8.9, -1.81, 0.2],
+#                 [1.41, 1.051, 0.026]]
+
+# # E = 2.71828218284 #Euler
+
+# exp_values = np.exp(layer_outputs)
+
+# print(np.sum(layer_outputs, axis=1, keepdims=True))
+
+
+# norm_values = exp_values / np.sum(exp_values)
+# exp_values = []
+
+# for output in layer_outputs: #Exponentiation to get rid of negatives 
+#     exp_values.append(E**output)
+
+# print(exp_values)
+
+#Normalization of the sum of exp values
+# norm_base = sum(exp_values) 
+# norm_values = []
+
+# for value in exp_values:
+#     norm_values.append(value / norm_base)
+
+# print(norm_values)
+# print(sum(norm_values))
+
+
+
+
 
 X = [[1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]]
 
-
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
-
+X, y = spiral_data(100, 3) #100 data sets of 3 classes
 
 
 class Layer_Dense:
@@ -28,13 +65,45 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(4, 5) #Input size X is 4 and output = 5
-layer2 = Layer_Dense(5, 2) #output from layer 1 is the input for layer 2 therefore input = 5
+layer1 = Layer_Dense(2, 5) #Input size X is 4 and output = 5
+activation1 = Activation_ReLU() # Produce the activation for the entire layer
+# layer2 = Layer_Dense(5, 2) #output from layer 1 is the input for layer 2 therefore input = 5
 
 layer1.forward(X)
-# print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)
+
+
+
+
+
+
+
+ # See for code: https://gist.github.com/Sentdex/454cb20ec5acf0e76ee8ab8448e6266c
+# def create_data(points, classes): # points = how many data point sets you want/ how many feature sets per how many classes you want
+#     X = np.zeros((points*classes, 2))
+#     y = np.zeros(points*classes, dtype='uint8')
+#     for class_number in range(classes):
+#         ix = range(points*class_number, points*(class_number+1))
+#         r = np.linspace(0.0, 1, points) #Radius
+#         t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+#         X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+#         y[ix] = class_number
+#     return X, y
+
+
+# import matplotlib.pyplot as plt
+
+# X, y = create_data (100, 3)
+# plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+# plt.show()
+
+
+
+
+# # print(layer1.output)
+# layer2.forward(layer1.output)
+# print(layer2.output)
 
 
 
@@ -96,9 +165,13 @@ print(layer2.output)
 
 # print (layer_outputs)
 
+# inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+# output = []
+
 #OutputC
 # Output = input * weight + bias
 # output = [inputs[0] * Weights1[0] + inputs[1] * Weights1[1] + inputs[2] * Weights1[2] + inputs[3] * Weights1[3] + bias1, #Modeling a Layer (3 Neurons)
 #           inputs[0] * Weights2[0] + inputs[1] * Weights2[1] + inputs[2] * Weights2[2] + inputs[3] * Weights2[3] + bias2, #Each neuron has its own unique weight set
 #           inputs[0] * Weights3[0] + inputs[1] * Weights3[1] + inputs[2] * Weights3[2] + inputs[3] * Weights3[3] + bias3] #and bias therefore it's unique output
 #           #Struggle with deep learning is finding how to best tweak the bias and weights
+
